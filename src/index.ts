ea71fdd1b2ba928437e8a0fa9891f0a565a67887
Ezra Mollena <ezra.mollena@lanexcorp.com>
2025-07-04 11:15:04 +0800

    trigger deployment used eip

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_req: Request, res: Response) => {
-  res.send("Productivity Tracker API is running!");
+  res.send("Workflow Insight API is running!");
 });
 
 app.post("/webhook", async (req: Request, res: Response) => {
